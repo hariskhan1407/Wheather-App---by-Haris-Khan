2721dac9ae25828def50250ace549a2c949b66b4
hariskhan1407 <148951890+hariskhan1407@users.noreply.github.com>
2023-11-23 23:30:00 +0530

    added region and contry also.

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ function cheakWheather(city){
     .then(res=> res.json())
     // .then(data => console.log(data))
     .then(data => {
-        
         document.querySelector(".city").innerHTML = data.location.name
+        document.querySelector(".region").innerHTML = data.location.region
+        document.querySelector(".country").innerHTML = data.location.country
         document.querySelector(".humidity").innerHTML = data.current.humidity+'km/h'
         document.querySelector(".windspeed").innerHTML = data.current.wind_kph+'km/h'
-        document.querySelector(".temp").innerHTML = data.current.temp_c+'°C'
+        document.querySelector(".temp").innerHTML = data.current.temp_c+'°C'       
 
 
         
     })
     .catch(error => {
         document.querySelector(".city").innerHTML = "Kuch to Sharm Karo😂"
+        document.querySelector(".region").innerHTML = ""
+        document.querySelector(".country").innerHTML = ""
         document.querySelector(".humidity").innerHTML = '0 km/h'
         document.querySelector(".windspeed").innerHTML = '0 km/h'
         document.querySelector(".temp").innerHTML = 'Areeee!'
